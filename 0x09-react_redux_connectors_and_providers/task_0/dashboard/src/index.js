0ce8dd3d188531898855bbde8322d0d16b31f50b
Edgar Nyamekye Agongo <kwakuedgar@gmail.com>
2024-09-30 21:41:28 +0000

    React Redux Connectors and Providers

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import App from "./App/App";
+import uiReducer, { initialState } from "./reducers/uiReducer";
+import { Map } from "immutable";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-	<React.StrictMode>
-	<App />
-	</React.StrictMode>
+const store = createStore(uiReducer, Map(initialState));
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
+
